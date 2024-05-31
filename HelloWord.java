0579cd65cd454a6e69cf950a026316a7aa32a73f
RandriamihajaOlivierMartial <randriamhajamartial@gmail.com>
2024-05-31 08:53:04 +0200

    teste

---
+++ HelloWord.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author Martial
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        // Afficher le message "Bonjour le monde !" à la console
+        System.out.println("Bonjour le monde !");
+    }
+}
+
+    
+
